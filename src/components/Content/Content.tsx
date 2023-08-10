452adf4490a72183c4b108006454ce6bd4ff3e8b
Vladyslav Ivashko <vlad.ivashko72@gmail.com>
2023-08-10 20:41:50 +0300

    implement user search

--- src/components/Content/Content.tsx
+++ src/components/Content/Content.tsx
@@ -1,15 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAppDispatch } from "../../hooks/useAppDispatch";
 import SearchBar from "../SearchBar/SearchBar";
 import UserCard from "../UserCard/UserCard";
 import Styles from "./content.module.scss";
 import classNames from "classnames/bind";
 import { fetchUsers } from "../../store/actions/fetchUsers";
+import { useSelector } from "react-redux";
 
 const cx = classNames.bind(Styles);
 
 const Content = () => {
   const dispatch = useAppDispatch();
+  const [query, setQuery] = useState("");
 
   useEffect(() => {
     dispatch(fetchUsers("d"));
@@ -17,7 +19,8 @@ const Content = () => {
 
   return (
     <div className={cx("content", "content__container")}>
-      <SearchBar />
+      <SearchBar searchUsers={setQuery} />
+      Query: {query}
       <UserCard />
     </div>
   );
--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,5 +1,38 @@
-const SearchBar = () => {
-  return <div>Search bar</div>;
+import { FC, useRef } from "react";
+import Styles from "./searchBar.module.scss";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(Styles);
+
+interface SearchBarProps {
+  searchUsers: (query: string) => void;
+}
+
+const SearchBar: FC<SearchBarProps> = ({ searchUsers }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const submitSearch = () => {
+    console.log(inputRef.current?.value);
+    const searchValue = inputRef.current?.value as string;
+    searchUsers(searchValue);
+  };
+
+  return (
+    <div className={cx("searchBar", "searchBar__container")}>
+      <input
+        ref={inputRef}
+        placeholder="Search by name"
+        className={cx("searchBar__input")}
+      />
+      <button
+        type="button"
+        onClick={submitSearch}
+        className={cx("searchBar__button")}
+      >
+        Search
+      </button>
+    </div>
+  );
 };
 
 export default SearchBar;
